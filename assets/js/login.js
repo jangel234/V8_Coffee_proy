72abbdaa2a0e7b4514c57a679a01b3e9cd3517f8
Jesus Angel Aguilar <123581561+Angelk234@users.noreply.github.com>
2025-05-06 21:49:49 -0600

    I create first connection to database and solve some issues in sql, also add login

---
+++ assets/js/login.js
@@ -0,0 +1,30 @@
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const usuario = document.getElementById("username").value.trim();
+    const telefono = document.getElementById("telephone").value.trim();
+
+    console.log(so)
+    if (!usuario || !telefono) {
+        alert('Por favor rellene los campos');
+        return;
+    }
+
+    try {
+        const response = await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ usuario, telefono })
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) throw new Error(data.error || 'Error en el login');
+
+
+        console.log(data);
+
+    } catch (error) {
+        alert(error.message);
+    }
+});
---
+++ assets/js/server.js
@@ -0,0 +1,78 @@
+
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const app = express();
+
+app.use(cors()); // Permite que el navegador acceda a la API
+app.use(express.json()); // Permite recibir JSON
+
+// Configurar conexión a MySQL
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', // your user
+  password: 'angxd.com', // your password
+  database: 'V8Coffee' // name of your database
+});
+
+// Conexion a la base de datos
+connection.connect(error => {
+  if (error) {
+    console.error('Error al conectar a la base de datos:', error);
+    return;
+  }
+  console.log('Conexión exitosa a la base de datos');
+});
+
+// solicitudes
+// //
+// //
+// //
+
+//obtener usuarios
+//en desuso
+app.get('/users', (req, res) => {
+  connection.query('SELECT nombre FROM users', (error, results) => {
+    if (error) {
+      res.status(500).json({ error: error.message });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+//login
+app.post('/login', (req, res) => {
+  const { usuario, telefono } = req.body;
+
+  if (!usuario || !telefono) {
+    return res.status(400).json({ error: 'Usuario y contraseña requeridos' });
+  }
+
+  connection.query(
+    'SELECT * FROM Usuarios WHERE nombre = BINARY ? AND telefono = BINARY ?',
+    [usuario, telefono],
+    (error, results) => {
+      if (error) return res.status(500).json({ error: error.message });
+
+      if (results.length === 0) {
+        return res.status(401).json({ error: 'Credenciales inválidas' });
+      }
+
+      const user = results[0];
+
+      res.json({
+        success: true,
+        message: 'Login exitoso',
+        user
+      });
+    }
+  );
+});
+
+
+// Iniciar servidor
+app.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000');
+  });
+  
