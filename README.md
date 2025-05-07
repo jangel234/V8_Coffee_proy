e6de1d68e85d38f09a6313baadbd69f34d7cbdc4
Jesus Angel Aguilar <123581561+Angelk234@users.noreply.github.com>
2025-05-06 21:51:28 -0600

    I install node, cors ,express and mysql2

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # V8_Coffee_proy
 Is a proyect to 3rd partial of 'Programacion para web'
+
+
+
+1.- Change log in server.js
+
+2.- install sql2, cors and express
+npm i mysql2
+npm i cors
+npm i express
+
+3.- inicias node server.js
+
--- database.sql
+++ database.sql
@@ -1,3 +1,6 @@
+drop database if exists V8Coffee;
+create database V8Coffee;
+use V8Coffee;
 -- Creación de tablas
 CREATE TABLE Cliente (
     id int PRIMARY KEY AUTO_INCREMENT,
@@ -15,7 +18,7 @@ CREATE TABLE Usuarios (
 CREATE TABLE Insumos (
     id int PRIMARY KEY AUTO_INCREMENT,
     nombre varchar(100) NOT NULL,
-    costo varchar(10,2) NOT NULL
+    costo decimal(10,2) NOT NULL
 );
 
 CREATE TABLE Productos (
@@ -29,11 +32,11 @@ CREATE TABLE Productos (
 
 CREATE TABLE PI (
     id int PRIMARY KEY AUTO_INCREMENT,
-    id_Produto int NOT NULL,
+    id_Producto int NOT NULL,
     id_Insumo int NOT NULL,
     subtotal decimal(10,2),
-    FOREIGN KEY (id_Produto) REFERENCES Products(id),
-    FOREIGN KEY (id_Insumo) REFERENCES Insumos(id)
+    FOREIGN KEY (id_Producto) REFERENCES Productos (id),
+    FOREIGN KEY (id_Insumo) REFERENCES Insumos (id)
 );
 
 CREATE TABLE Pedidos (
@@ -68,11 +71,11 @@ INSERT INTO Insumos (nombre, costo) VALUES
 ('Leche entera', 12.00),
 ('Chocolate en polvo', 8.75);
 
-INSERT INTO Products (nombre, procedimientos, tamanio, HC, precio) VALUES
+INSERT INTO Productos (nombre, procedimientos, tamanio, HC, precio) VALUES
 ('Cappuccino Clásico', 'Preparado con doble shot de espresso', 'Mediano', 250, 45.00),
 ('Mocha Blanco', 'Mezcla de chocolate blanco y espresso', 'Grande', 320, 55.00);
 
-INSERT INTO Producto_Insumo (id_Produto, id_Insumo, subtotal) VALUES
+INSERT INTO PI (id_Producto, id_Insumo, subtotal) VALUES
 (1, 1, 15.50),
 (1, 2, 12.00),
 (2, 1, 15.50),
@@ -82,6 +85,6 @@ INSERT INTO Pedidos (Fecha, id_Cliente, id_Empleado, total) VALUES
 (NOW(), 1, 1, 100.00),
 (NOW(), 2, 2, 120.50);
 
-INSERT INTO PPI (id_PI, id_Pedido) VALUES
+INSERT INTO PP (id_PI, id_Pedido) VALUES
 (1, 1),
 (3, 2);
